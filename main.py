78f29b488ebc3faa997b47561425994a4868c427
jidneshmumbaikar25 <jidnesh.mumbaikar25@aiml.sce.edu.in>
2026-08-13 12:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import math
-
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)
+print("Jidnesh")
